50ac643b321f2fb9124f1a163d611e6ae2bb9a19
dkchv <anton@dkchv.ru>
2020-05-05 00:06:31 +0300

    Updates

---
+++ precache-manifest.b1f84e6b132bd9d45d7210330f753412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8ea341bbdfd4dd830d877db21b6fc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24402c30a0bee465abcb",
+    "url": "/static/css/main.3bfcc50d.chunk.css"
+  },
+  {
+    "revision": "e1c2486d998fc3123ad1",
+    "url": "/static/js/2.e5707793.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e5707793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24402c30a0bee465abcb",
+    "url": "/static/js/main.3ec22380.chunk.js"
+  },
+  {
+    "revision": "73557deebd79081c55e1",
+    "url": "/static/js/runtime-main.07871889.js"
+  }
+]);
